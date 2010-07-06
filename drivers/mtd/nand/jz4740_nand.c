37fc9c51b40103b1e4aab690b0dd72d1ba98f48f
Maarten ter Huurne <maarten@treewalker.org>
2010-07-06 14:07:43 +0200

    jz4740: nand: Repurpose eccpos to contain the offset of the first ECC byte in the oob.
    The eccpos array is only 64 bytes long, while 4K page NANDs need 72 bytes of ECC.
    This is a temporary measure until upstream has a solution.

--- drivers/mtd/nand/jz4740_nand.c
+++ drivers/mtd/nand/jz4740_nand.c
@@ -251,7 +251,7 @@ static int jz_nand_read_page_hwecc_oob_first(struct mtd_info *mtd,
 	int eccbytes = chip->ecc.bytes;
 	int eccsteps = chip->ecc.steps;
 	uint8_t *p = buf;
-	unsigned int ecc_offset = chip->page_shift;
+	unsigned int ecc_offset = chip->ecc.layout->eccpos[0];
 
 	/* Read the OOB area first */
 	chip->cmdfunc(mtd, NAND_CMD_READOOB, 0, page);
@@ -281,7 +281,7 @@ static void jz_nand_write_page_hwecc(struct mtd_info *mtd,
 	int eccbytes = chip->ecc.bytes;
 	int eccsteps = chip->ecc.steps;
 	const uint8_t *p = buf;
-	unsigned int ecc_offset = chip->page_shift;
+	unsigned int ecc_offset = chip->ecc.layout->eccpos[0];
 
 	for (i = ecc_offset; eccsteps; eccsteps--, i += eccbytes, p += eccsize) {
 		chip->ecc.hwctl(mtd, NAND_ECC_WRITE);
@@ -387,6 +387,9 @@ static int __devinit jz_nand_probe(struct platform_device *pdev)
 		pdata->ident_callback(pdev, chip, &pdata->partitions,
 					&pdata->num_partitions);
 	}
+
+	if (chip->ecc.layout->eccpos[0] == 0)
+		chip->ecc.layout->eccpos[0] = chip->page_shift;
 
 	ret = nand_scan_tail(mtd);
 	if (ret) {
