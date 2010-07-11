a619c1179019055ade667246d34be12a246d2a3d
Lars-Peter Clausen <lars@metafoo.de>
2010-07-11 21:38:17 +0200

    mmc: jz4740: fix typo

--- drivers/mmc/host/jz4740_mmc.c
+++ drivers/mmc/host/jz4740_mmc.c
@@ -763,7 +763,7 @@ static int __devinit jz4740_mmc_probe(struct platform_device* pdev)
 
 	host->clk = clk_get(&pdev->dev, "mmc");
 	if (IS_ERR(host->clk)) {
-		ret = PTR_ERR(host->clock);
+		ret = PTR_ERR(host->clk);
 		dev_err(&pdev->dev, "Failed to get mmc clock: %d\n", ret);
 		goto err_free_host;
 	}
