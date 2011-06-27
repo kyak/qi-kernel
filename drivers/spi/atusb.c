a7a8c8ed146546dc5a7f25d377374af8ed2095c5
Stefan Schmidt <stefan@datenfreihafen.org>
2011-06-27 02:23:28 +0200

    spi/atusb: Cleanup header includes
    Signed-off-by: Stefan Schmidt <stefan@datenfreihafen.org>

--- drivers/spi/atusb.c
+++ drivers/spi/atusb.c
@@ -11,12 +11,10 @@
  */
 
 #include <linux/kernel.h>
-#include <linux/errno.h>
-#include <linux/init.h>
-#include <linux/slab.h>
 #include <linux/module.h>
+#include <linux/platform_device.h>
+#include <linux/interrupt.h>
 #include <linux/usb.h>
-
 #include <linux/spi/spi.h>
 #include <linux/spi/at86rf230.h>
 
