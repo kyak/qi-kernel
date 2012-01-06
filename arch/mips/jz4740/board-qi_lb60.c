d6871a8333af99bfe7505fc3b1222c340530c850
Axel Lin <axel.lin@gmail.com>
2012-01-06 11:30:10 +0800

    ASoC: jz4740: Convert qi_lb60 to use snd_soc_register_card()
    Use snd_soc_register_card() instead of creating a "soc-audio" platform device.
    
    Signed-off-by: Axel Lin <axel.lin@gmail.com>

--- arch/mips/jz4740/board-qi_lb60.c
+++ arch/mips/jz4740/board-qi_lb60.c
@@ -417,6 +417,11 @@ static struct platform_device qi_lb60_charger_device = {
 	},
 };
 
+/* audio */
+static struct platform_device qi_lb60_audio_device = {
+	.name = "qi-lb60-audio",
+	.id = -1,
+};
 
 static struct platform_device *jz_platform_devices[] __initdata = {
 	&jz4740_udc_device,
@@ -433,6 +438,7 @@ static struct platform_device *jz_platform_devices[] __initdata = {
 	&qi_lb60_gpio_keys,
 	&qi_lb60_pwm_beeper,
 	&qi_lb60_charger_device,
+	&qi_lb60_audio_device,
 };
 
 static void __init board_gpio_setup(void)
